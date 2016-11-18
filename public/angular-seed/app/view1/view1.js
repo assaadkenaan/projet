474f7eb62a46c63d53ba5cab7846896572a23f2b
assaad.r.kenaan <assaad.r.kenaan@gmail.com>
2016-11-18 18:03:12 -0500

    message

--- public/angular-seed/app/view1/view1.js
+++ public/angular-seed/app/view1/view1.js
@@ -1,31 +1,14 @@
 'use strict';
-
-angular.module('myApp.view1', ['ngRoute','ngResource'])
-
+angular.module('myApp.view1', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
-$routeProvider.when('/view1', {
-  templateUrl: 'view1/view1.html',
-  controller: 'View1Ctrl'
-});
-
-}])
-
-.config(['$resourceProvider', function($resourceProvider) {
- $resourceProvider.defaults.stripTrailingSlashes = false;
-}])
-
-/*  .factory('Student', function($resource) {
-   var Student = $resource('/students/:id.json', {id: '@id'});
-   
-   .controller('View1Ctrl',function($scope, Student) {
-  $scope.students = Student.query(); 
-
-});
+ $routeProvider.when('/view1', {
+   templateUrl: 'view1/view1.html',
+   controller: 'View1Ctrl'
+ });
  
-}) */
-
+}])
 .controller('View1Ctrl',['$scope','$resource',function($scope, $resource) {
- var Student = $resource('/students/:id.json', {id: '@id'});
- $scope.students = Student.query();
-
+  var Student = $resource('/students/:id.json', {id: '@id'});
+  $scope.students = Student.query();
+ 
 }]);

108a884d362cfb0df57a8a54995dcefe72d32bcd
assaad.r.kenaan <assaad.r.kenaan@gmail.com>
2016-11-21 16:24:39 -0500

    Message

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,8 @@
 class City < ActiveRecord::Base
-  before_create :geocode
+  validates :lat, presence: true 
+  validates :long, presence: true 
+
+  before_validation :geocode
 
    public
  
@@ -17,8 +20,9 @@ class City < ActiveRecord::Base
 
   def geocode
     places = Nominatim.search.city(self.name).limit(1)
+    if places.first
     self.lat=places.first.lat
     self.long=places.first.lon
   end
-
+  end
 end

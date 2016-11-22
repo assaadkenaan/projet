f26ae3ddf52a343d1a1ea99a4669a90a14b6e6cc
assaad.r.kenaan <assaad.r.kenaan@gmail.com>
2016-11-22 09:44:33 -0500

    MessageToTravis

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,6 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require "simplecov"
 SimpleCov.start
-
 VCR.configure do |config|
 config.cassette_library_dir = "fixtures/vcr_cassettes"
 config.hook_into :faraday # or :fakeweb
